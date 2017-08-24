6b5ddf68d5ae05cf868b2f23692b3ce05ffecaf9
Ather Tahir <athertahir1995@gmail.com>
2017-08-24 10:59:15 +0500

    port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var selectedbutton='';
 var count =0;
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.port || process.env.PORT || 3000, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
