897b5f58069859d318c27d3a93b68db9a6735bdc
Ather Tahir <athertahir1995@gmail.com>
2017-08-26 12:01:25 +0500

    demo_without_azure

--- app.js
+++ app.js
@@ -27,9 +27,7 @@ selectedbutton = session.message.text;
     	// Echo back users text
 		if (!session.privateConversationData[login]) {
 			session.beginDialog('login');
-			//while()
-			session.send('Hi there. I m PivBot, an IT automation and Artificial Intelligence (AI) service provided by Catalyst Technology Group to help you perform a variety of IT tasks. Please choose an area to get started');	
-			getMainMenuHeroCard(session);
+
 		}
 		else
 		{
--- login.js
+++ login.js
@@ -27,6 +27,26 @@ module.exports = [
 ]
 function userlogin(session) {
 		session.privateConversationData['login'] = true;
-		//session.privateConversationData['signing']=true;
 		session.send('login Successfull');
+		session.send('Hi there. I m PivBot, an IT automation and Artificial Intelligence (AI) service provided by Catalyst Technology Group to help you perform a variety of IT tasks. Please choose an area to get started');	
+		getMainMenuHeroCard(session);
+}
+function createMainMenuHeroCard(session) {
+    return new builder.HeroCard(session)
+        .title('Select your choice')
+		.buttons([
+            builder.CardAction.postBack(session, 'Bot.Command.MainMenu.NodeBot1', 'Accounts Receivable'),
+			builder.CardAction.postBack(session, 'Bot.Command.MainMenu.NodeBot2', 'Customer Service'),
+			builder.CardAction.postBack(session, 'Bot.Command.MainMenu.NodeBot3', 'HR'),
+			builder.CardAction.postBack(session, 'Bot.Command.MainMenu.NodeBot4', 'Marketing'),
+			builder.CardAction.postBack(session, 'Bot.Command.MainMenu.NodeBot5', 'Operations'),
+			builder.CardAction.postBack(session, 'Bot.Command.MainMenu.NodeBot6', 'Sales'),
+			builder.CardAction.postBack(session, 'Bot.Command.MainMenu.NodeBot7', 'Service')			
+        ]);
+}
+function getMainMenuHeroCard(session) {
+		var card = createMainMenuHeroCard(session);
+        // attach the card to the reply message
+        var msg = new builder.Message(session).addAttachment(card);
+        session.send(msg);
 }
